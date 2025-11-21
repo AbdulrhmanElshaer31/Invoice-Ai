2c7ae13472e0a66fbc2d0f1110402b1fa9be60ea
AbdulrhmanElshaer31 <abdelrhmanelshaer31@gmail.com>
2025-11-21 21:34:38 +0200

    EveryThing Good Now !

--- app/(pages)/cost-center/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ConstCenter() {
-  return (
-    <div>ConstCenter</div>
-  )
-}
-
-export default ConstCenter
--- app/(pages)/expense-type/page.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ExpenseType() {
-  return (
-    <div>ExpenseType</div>
-  )
-}
-
-export default ExpenseType
